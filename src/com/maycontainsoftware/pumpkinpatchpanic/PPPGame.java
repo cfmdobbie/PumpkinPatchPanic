9b2686111eb8dcd847a5f3bb4912585fceec0ce8
cfmdobbie <cfmdobbie@gmail.com>
2014-10-12 22:01:01 +0100

    Stub ad visibility code

--- src/com/maycontainsoftware/pumpkinpatchpanic/PPPGame.java
+++ src/com/maycontainsoftware/pumpkinpatchpanic/PPPGame.java
@@ -15,20 +15,25 @@ public class PPPGame implements ApplicationListener {
 	private SpriteBatch batch;
 	private Texture texture;
 	private Sprite sprite;
+	private final IAdVisibilityCallback adVisibilityCallback;
 	
+	public PPPGame(IAdVisibilityCallback adVisibilityCallback) {
+		this.adVisibilityCallback = adVisibilityCallback;
+	}
+
 	@Override
-	public void create() {		
+	public void create() {
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
-		
+
 		camera = new OrthographicCamera(1, h/w);
 		batch = new SpriteBatch();
-		
+
 		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
 		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
+
 		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
+
 		sprite = new Sprite(region);
 		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
 		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
@@ -40,12 +45,20 @@ public class PPPGame implements ApplicationListener {
 		batch.dispose();
 		texture.dispose();
 	}
+	
+	private boolean adVisible = true;
 
 	@Override
-	public void render() {		
+	public void render() {
+		
+		if(Gdx.input.isTouched()) {
+			adVisible = !adVisible;
+			adVisibilityCallback.setAdVisible(adVisible);
+		}
+		
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
+
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		sprite.draw(batch);
