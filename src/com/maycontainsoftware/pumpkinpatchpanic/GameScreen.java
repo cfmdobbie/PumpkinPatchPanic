3bab51cfc8d6afeccf5a1b2a435a8db284ea046d
cfmdobbie <cfmdobbie@gmail.com>
2014-10-28 14:16:01 +0000

    Clean up imports

--- src/com/maycontainsoftware/pumpkinpatchpanic/GameScreen.java
+++ src/com/maycontainsoftware/pumpkinpatchpanic/GameScreen.java
@@ -14,7 +14,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 /**
  * Game screen.
